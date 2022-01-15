7143039a2dbf8f0730f2bae12baa08e3da520402
ArturMavlidov <artur.mavlidov75@mail.ru>
2022-01-15 17:23:53 +0300

    Adaptive layout

--- src/components/GoodsItem/index.jsx
+++ src/components/GoodsItem/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import './style.css'
+import "./style.css";
 
 export default function GoodsItem(props) {
   const {
@@ -14,30 +14,34 @@ export default function GoodsItem(props) {
   const { full_background: background } = props.displayAssets[0];
 
   return (
-    <div className="card">
-      <div className="card-image">
-        <img src={background} alt={name} />
-      </div>
-      <div className="card-content">
-        <span className="card-title">{name || "Название"}</span>
-        <p>{description || "Описания пока что нет"}</p>
-      </div>
-      <div className="card-action">
-        <button
-          className="btn"
-          onClick={() =>
-            addToBasket({
-              id,
-              name,
-              price,
-            })
-          }
-        >
-          Купить
-        </button>
-        <span className="right" style={{ fontSize: "1.8rem" }}>
-          {price} руб.
-        </span>
+    <div className="row">
+      <div className="col s12 m12">
+        <div className="card">
+          <div className="card-image">
+            <img src={background} alt={name} />
+          </div>
+          <div className="card-content">
+            <span className="card-title">{name || "Название"}</span>
+            <p>{description || "Описания пока что нет"}</p>
+          </div>
+          <div className="card-action">
+            <button
+              className="btn"
+              onClick={() =>
+                addToBasket({
+                  id,
+                  name,
+                  price,
+                })
+              }
+            >
+              Купить
+            </button>
+            <span className="right" style={{ fontSize: "1.8rem" }}>
+              {price} руб.
+            </span>
+          </div>
+        </div>
       </div>
     </div>
   );
